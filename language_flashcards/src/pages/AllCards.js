4d4840297a941231ce349f7261f6bf281a296729
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2025-02-28 15:34:55 -0700

    added allcards and starredcards

---
+++ language_flashcards/src/pages/AllCards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllCards = () => {
+  return <h1>All Flashcards</h1>;
+};
+
+export default AllCards;
---
+++ language_flashcards/src/pages/StarredCards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StarredCards = () => {
+  return <h1>Starred Flashcards</h1>;
+};
+
+export default StarredCards;
