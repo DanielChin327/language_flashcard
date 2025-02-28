9c4a885a9460592d6453ec23a02d85763719f625
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2025-02-27 22:40:18 -0700

    added data.js

---
+++ language_flashcards/src/data.js
@@ -0,0 +1,6 @@
+export const flashcards = [
+  { id: 1, japanese: "猫", korean: "고양이", definition: "Cat", starred: false },
+  { id: 2, japanese: "犬", korean: "개", definition: "Dog", starred: false },
+  { id: 3, japanese: "本", korean: "책", definition: "Book", starred: false },
+  { id: 4, japanese: "車", korean: "차", definition: "Car", starred: false }
+];
