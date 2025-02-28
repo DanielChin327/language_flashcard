951a6264e5741b6f986e48c23087935ed5a4cca3
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2025-02-28 00:40:21 -0700

    working on home.js

---
+++ language_flashcards/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Language Flashcards</h1>
+      <p>Create and review your flashcards here!</p>
+    </div>
+  );
+};
+
+export default Home;
